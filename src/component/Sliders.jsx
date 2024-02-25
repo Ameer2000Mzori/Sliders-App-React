7cb4569305af4d858d117ce36033d24e8dce8a09
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-25 04:40:16 +0100

    added some html tree

--- src/component/Sliders.jsx
+++ src/component/Sliders.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { userData } from './hooks/usersData.js'
 const Sliders = () => {
-  return <div>Sliders</div>
+  return (
+    <>
+      <div>
+        <h1>Sliders</h1>
+        <div>
+          <div>
+            {userData.map((slider) => {
+              console.log(slider)
+            })}
+          </div>
+
+          <button>NEXT</button>
+          <button>PREV</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Sliders
